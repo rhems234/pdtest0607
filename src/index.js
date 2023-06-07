8ba80194aa1767be52369f652120b9388d24f644
rhems234 <rhems234@gmail.com>
2023-06-07 12:34:16 +0900

    test4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, {useState, useCallback} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
